fe7f1b3ed6f5447ce5feff4aeecea9331637eb3d
dukong <junggg097@daum.net>
2024-01-11 15:49:20 +0900

    README.md 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- 게시판 더미 데이터
+-- 게시판 데이터
 INSERT INTO board(name)
 VALUES ('자유 게시판');
 INSERT INTO board(name)
@@ -8,9 +8,8 @@ VALUES ('일상 게시판');
 INSERT INTO board(name)
 VALUES ('사건 사고 게시판');
 
-
--- 게시글 더미 데이터
-INSERT INTO Article(board_id, title, content, writer, password)
+-- 게시글 데이터
+INSERT INTO article(board_id, title, content, writer, password)
 VALUES (1, '자유롭게', '자유로웡~~~', '유진', '1111');
 INSERT INTO article(board_id, title, content, writer, password)
 VALUES (2, 'SPRING BOOT 프로젝트 .. ', '살짝 어려운 거 같기도 하고 ,, 댓글은 잘 못하겠고 ,,', '유진', '1111');
